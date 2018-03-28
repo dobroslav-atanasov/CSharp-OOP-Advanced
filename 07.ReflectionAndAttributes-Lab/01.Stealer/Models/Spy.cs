a4609d5bb9c16ed96462c5e0fc1293a66087ee9c
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-03-28 11:43:58 +0300

    Modify Spy.cs. Added BindingFlags.Static.

--- 07.ReflectionAndAttributes-Lab/01.Stealer/Models/Spy.cs
+++ 07.ReflectionAndAttributes-Lab/01.Stealer/Models/Spy.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Reflection;
 using System.Text;
 
@@ -8,12 +7,12 @@ public class Spy
     public string StealFieldInfo(string className, params string[] fieldsNames)
     {
         Type classType = Type.GetType(className);
-        FieldInfo[] fieldInfos = classType.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+        FieldInfo[] fieldInfos = classType.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
 
         StringBuilder sb = new StringBuilder();
         sb.AppendLine($"Class under investigation: {className}");
 
-        Hacker instance = (Hacker)Activator.CreateInstance(classType, new object[] { });
+        Object instance = Activator.CreateInstance(classType, new object[] { });
         foreach (FieldInfo info in fieldInfos)
         {
             foreach (string name in fieldsNames)
