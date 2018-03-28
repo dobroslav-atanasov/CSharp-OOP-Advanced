64f75ef5b4a35ff6757635e6665970048acf6208
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-03-28 11:07:17 +0300

    Uploaded Reflection and Attributes - Lab.

---
+++ 07.ReflectionAndAttributes-Lab/01.Stealer/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.Stealer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
