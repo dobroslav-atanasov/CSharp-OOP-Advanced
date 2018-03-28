63317edb991fe2789b4d436215ccfff1eb10c7f7
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-03-28 12:00:47 +0300

    Uploaded problem 2.High Quality Mistakes.

---
+++ 07.ReflectionAndAttributes-Lab/02.HighQualityMistakes/StartUp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        Spy spy = new Spy();
+        string investigation = spy.AnalyzeAcessModifiers("Hacker");
+        Console.WriteLine(investigation);
+    }
+}
