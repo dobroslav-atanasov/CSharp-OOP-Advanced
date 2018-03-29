a136a22e8b3db741727b966a0ed210057a4cded7
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-03-29 09:38:16 +0300

    Uploaded problem 3 and 4 from Reflection and Attributes - Lab.

---
+++ 07.ReflectionAndAttributes-Lab/03.MissionPrivateImpossible/Models/Spy.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+public class Spy
+{
+    public string RevealPrivateMethods(string className)
+    {
+        Type classType = Type.GetType(className);
+        MethodInfo[] methodInfos = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine($"All Private Methods of Class: {className}")
+            .AppendLine($"Base Class: {classType.BaseType.Name}");
+        foreach (MethodInfo method in methodInfos)
+        {
+            sb.AppendLine(method.Name);
+        }
+
+        return sb.ToString().Trim();
+    }
+
+    public string AnalyzeAcessModifiers(string className)
+    {
+        Type classType = Type.GetType(className);
+        FieldInfo[] fieldInfos = classType.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public);
+        MethodInfo[] methodInfos = classType.GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+
+        StringBuilder sb = new StringBuilder();
+        foreach (FieldInfo field in fieldInfos)
+        {
+            sb.AppendLine($"{field.Name} must be private!");
+        }
+        foreach (MethodInfo method in methodInfos.Where(m => m.Name.StartsWith("get") && m.IsPrivate))
+        {
+            sb.AppendLine($"{method.Name} have to be public!");
+        }
+        foreach (MethodInfo method in methodInfos.Where(m => m.Name.StartsWith("set") && m.IsPublic))
+        {
+            sb.AppendLine($"{method.Name} have to be private!");
+        }
+
+        return sb.ToString().Trim();
+    }
+
+    public string StealFieldInfo(string className, params string[] fieldsNames)
+    {
+        Type classType = Type.GetType(className);
+        FieldInfo[] fieldInfos = classType.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine($"Class under investigation: {className}");
+
+        Object instance = Activator.CreateInstance(classType, new object[] { });
+        foreach (FieldInfo field in fieldInfos)
+        {
+            foreach (string name in fieldsNames)
+            {
+                if (field.Name == name)
+                {
+                    sb.AppendLine($"{field.Name} = {field.GetValue(instance)}");
+                }
+            }
+        }
+
+        return sb.ToString().Trim();
+    }
+}
---
+++ 07.ReflectionAndAttributes-Lab/03.MissionPrivateImpossible/StartUp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        Spy spy = new Spy();
+        string investigation = spy.RevealPrivateMethods("Hacker");
+        Console.WriteLine(investigation);
+    }
+}
